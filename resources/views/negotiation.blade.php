1b9175f4e528b529da4a9426c9c41ea9f53580c0
Justin Bearder <justinabearder@gmail.com>
2021-05-19 10:04:06 +1000

    More code

--- resources/views/negotiation.blade.php
+++ resources/views/negotiation.blade.php
@@ -6,7 +6,8 @@
     <link rel="stylesheet" href="/assets/css/farmerslist.css">
     <link rel="stylesheet" href="/assets/css/negotiation.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <script src="/assets/js/negotiation.js"></script>
+
+    
 </head>
 
 <body>
@@ -20,38 +21,11 @@
         </div>
     </div>
     <div style="overflow-x:auto;">
-        <table id="example" style="text-align: center;">
-            <tr>
-                <th>Farmer</th>
-                <th>Product</th>
-                <th>Price</th>
-                <th>Qauntity / KGs</th>
-                <th>Status</th>
-            </tr>
-            <tr class="table-row" data-href="checkstatus()">
-                <td>Jill</td>
-                <td>Rice</td>
-                <td>20</td>
-                <td>50</td>
-                <td>Rejected</td>
-            </tr>
-            <tr class="table-row" data-href="checkstatus()">
-                <td>Bill</td>
-                <td>Tomatos</td>
-                <td>15</td>
-                <td>30</td>
-                <td>In progress</td>
-            </tr>
-            <tr class="table-row" data-href="checkstatus()">
-                <td>Jake</td>
-                <td>Potatos</td>
-                <td>8</td>
-                <td>68</td>
-                <td>Accepted</td>
-            </tr>
+        <table id="myTable">
         </table>
     </div>
 
 </body>
-
+<script src="/assets/js/negotiation.js"></script>
+<script src="/assets/js/negotiations1.js"></script>
 </html>
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\negotiations;
 use App\Http\Controllers\productsave;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,3 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post("product",[productsave::class,'prod']); 
+Route::get("negotiations/{negotiations}",[negotiations::class,'negotiate']); 
+// Route::get("negotiations",[negotiations::class,'negotiate']); 
